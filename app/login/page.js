f0054240402ad906df189d60fde1905e7b80187e
Diego López Ariza <diegoawesome2017@outlook.com>
2024-04-11 16:15:17 -0600

    Revert "RLS enabled on supabase"
    This reverts commit 84d39f4a6d8cba04046927cd41932dc2ecc9716a.

--- app/login/page.js
+++ app/login/page.js
@@ -1,61 +1,70 @@
-'use client';
-import { Button, Divider, Input } from "@nextui-org/react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { toastHandler } from "@/handlers/todos";
-import { signIn, signUp } from "@/database/auth";
-import { StoredContext } from "@/context";
-import { validateLogin } from "@/validations";
-import { useState } from "react";
+"use client"
+import { Button, Divider, Input } from "@nextui-org/react"
+import { signIn, signUp } from "@/database/auth"
+import { useState } from "react"
+import { StoredContext } from "@/context"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+import { toastHandler } from "@/handlers/todos"
+import { validateLogin } from "@/validations"
 
-export default function Login() {
-    const [loading, setLoading] = useState(false);
+export default function Login({ }) {
     const { setStored, memory: { loginForm, errors } } = StoredContext()
     const path = usePathname()
+    const [loading, setLoading] = useState(false)
+    const handleChange = ({ target }) => {
+        setStored({
+            loginForm: { ...loginForm, [target.name]: target.value },
+            errors: validateLogin(loginForm)
+        })
+    }
     const handleSignIn = ({ target }) => {
-        toastHandler(signIn(loginForm), setLoading, async ({ data: { session } }) => {
+        toastHandler(signIn(form), setLoading, async ({ data: { session } }) => {
             setStored({ session, loginForm: { email: '', password: '' } })
             target.reset()
-        },{
-            loading: 'Iniciando sesión...',
+        }, {
+            loading: 'Iniciando sesion...',
             success: 'Bienvenido',
         })
     }
     const handleSignUp = ({ target }) => {
-        toastHandler(signUp(loginForm), setLoading, async () => {
+        toastHandler(signUp(form), setLoading, () => {
             setStored({ loginForm: { email: '', password: '' } })
             target.reset()
-        },{
+        }, {
             loading: 'Registrando...',
-            success: 'Revise su correo para confirmar su cuenta',
+            success: 'Revisa tu correo para confirmar tu cuenta',
         })
-    }
-    const handleChange = ({ target }) => {
-        setStored({ loginForm: { ...loginForm, [target.name]: target.value }, errors: validateLogin(loginForm) })
     }
     const handleSubmit = (e) => {
         e.preventDefault()
+        setLoading(true)
         if (path == '/login') {
             handleSignIn(e)
             return
         }
         handleSignUp(e)
     }
+
     return (
-        <div>
+        <div className="w-96">
             <form className="flex flex-col gap-2" onChange={handleChange} onSubmit={handleSubmit}>
-                <Input isRequired isDisabled={loading} value={loginForm.email} errorMessage={errors.email} name="email" type="email" label="Email" />
-                <Input isRequired isDisabled={loading} value={loginForm.password} errorMessage={errors.password} name="password" type="password" label="Password" />
-                <Button type="submit" isLoading={loading} color="primary">{path == '/login' ? 'Iniciar sesión' : 'Registrarse'}</Button>
+                <Input isRequired name="email" type="email" label="Correo" isDisabled={loading} value={loginForm.email} errorMessage={errors.email}/>
+                <Input isRequired name="password" type="password" label="Contraseña" isDisabled={loading} value={loginForm.password} errorMessage={errors.password}/>
+                <Button type="submit" isLoading={loading} color="primary">{path == '/login' ? 'Iniciar sesion' : 'Registrar'}</Button>
                 <Divider className="my-1" />
                 <Link href={path == '/login' ? '/signup' : '/login'} legacyBehavior passHref>
-                    <Button>{path == '/login' ? 'Registrarse' : 'Iniciar sesión'}</Button>
+                    <Button>
+                        {path == '/login' ? 'Registrarse' : 'Iniciar sesión'}
+                    </Button>
                 </Link>
                 <Divider></Divider>
                 <Link href={'/mlink'} legacyBehavior passHref>
-                    <Button variant="light">Olvidé mi contraseña</Button>
+                    <Button variant="light" color="primary">
+                        Olvidé mi contraseña
+                    </Button>
                 </Link>
             </form>
         </div>
     )
-}
+} 
--- components/TooltipTodos.js
+++ components/TooltipTodos.js
@@ -1,4 +1,4 @@
-import { Button, Checkbox, CheckboxGroup, CircularProgress, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Tooltip, useDisclosure } from '@nextui-org/react'
+import { Button, Checkbox, CheckboxGroup, CircularProgress, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Tooltip, useDisclosure } from '@nextui-org/react'
 import { useEffect, useState } from 'react'
 import { SliderTodo } from './todo'
 import { StoredContext, createFilter, filterTodos } from '@/context'
@@ -23,27 +23,25 @@ export const TooltipTodos = () => {
         })
     }, [])
     return (
-        <>
-            <Tooltip className='text-foreground bg-background' closeDelay={session ? 500 : 5000} isOpen={open} onOpenChange={(open) => setOpen(open)} content={
-                session ? (
-                    <div>
-                        <CheckboxGroup label='Filtro' orientation="horizontal" onValueChange={setFilter}>
-                            <Checkbox radius="full" value='important' color="danger">Favoritas</Checkbox>
-                            <Checkbox radius="full" value='done'>Completadas</Checkbox>
-                        </CheckboxGroup>
-                        {loading ? <CircularProgress size="md" className="mt-5" /> : <SliderTodo todos={filterTodos(todos, filters)}></SliderTodo>}
-                    </div>
-                ) : ('Para ver las tareas antes tiene que iniciar sesion')
-            }>
-                <Button variant='light' color='danger' onClick={() => {
-                    setOpen((open) => !open)
-                }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
-                    </svg>
-                    Tareas</Button>
-            </Tooltip>
-        </>
+        <Tooltip className='text-foreground bg-background' closeDelay={session ? 500 : 5000} isOpen={open} onOpenChange={(open) => setOpen(open)} content={
+            session ? (
+                <div>
+                    <CheckboxGroup label='Filtro' orientation="horizontal" onValueChange={setFilter}>
+                        <Checkbox radius="full" value='important' color="danger">Favoritas</Checkbox>
+                        <Checkbox radius="full" value='done'>Completadas</Checkbox>
+                    </CheckboxGroup>
+                    {loading ? <CircularProgress size="md" className="mt-5" /> : <SliderTodo todos={filterTodos(todos, filters)}></SliderTodo>}
+                </div>
+            ) : ('Para ver las tareas antes tiene que iniciar sesion')
+        }>
+            <Button variant='light' color='danger' onClick={() => {
+                setOpen((open) => !open)
+            }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+                </svg>
+                Tareas</Button>
+        </Tooltip>
     )
 }
 export const ModalTodos = () => {
